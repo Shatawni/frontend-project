c2bb626d49c11f4fda2e9f31d56ca70f2eb2e5f3
Shatawni Wright <shatawni.johnson@gmail.com>
2022-04-28 21:58:20 -0400

    Final Final commit to frontend project

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ function loadPage() {
   var ul = document.createElement("ul");
   var button = document.getElementById("bestQuote");
 
-  // queue = new createjs.LoadQueue(false);
-  // queue.installPlugin(createjs.Sound);
-  // queue.addEventListener("complete", getBestQuote);
-  // queue.loadManifest([{ id: "sound", src: "HowSway.mp3" }]);
-
   document.getElementById("secondH1").style.visibility = "hidden";
   document.getElementById("memeContainer").style.visibility = "hidden";
   document.getElementById("ulContainer").style.visibility = "hidden";
@@ -35,21 +30,11 @@ function loadPage() {
         divUl.appendChild(ul);
         document.body.appendChild(divUl);
         document.querySelector("#ulContainer").style.visibility = "visible";
-        //createjs.Sound.addEventListener("loadComplete", getBestQuote);
       })
       .catch((err) => console.log(err));
   }
 
   function getBestQuote() {
-    // createjs.Sound.addEventListener("loadComplete", (e) => {
-    //   document.querySelector("body").style.visibility = "hidden";
-    //   document.querySelector("#secondH1").style.visibility = "visible";
-    //   var button = document.getElementById("refreshBtn");
-    //   document.getElementById("refreshBtn").style.visibility = "visible";
-    //   document.querySelector("#memeContainer").style.visibility = "visible";
-    //   document.querySelector("#ulContainer").style.visibility = "hidden";
-    //   createjs.Sound.play("sound");
-    // });
     var audio = new Audio("HowSway.mp3");
     audio.play();
     document.querySelector("body").style.visibility = "hidden";
@@ -58,6 +43,5 @@ function loadPage() {
     document.getElementById("refreshBtn").style.visibility = "visible";
     document.querySelector("#memeContainer").style.visibility = "visible";
     document.querySelector("#ulContainer").style.visibility = "hidden";
-    // createjs.Sound.play("sound");
   }
 }
